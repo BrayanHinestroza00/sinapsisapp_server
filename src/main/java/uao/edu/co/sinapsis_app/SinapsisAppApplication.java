7c4943b023a0742dfb885ace96750aba687d0ee7
Brayan Hinestroza <brayan.hinestroza@uao.edu.co>
2022-11-25 16:27:18 -0500

    Initialize project using Spring Boot

---
+++ src/main/java/uao/edu/co/sinapsis_app/SinapsisAppApplication.java
@@ -0,0 +1,13 @@
+package uao.edu.co.sinapsis_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinapsisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinapsisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uao/edu/co/sinapsis_app/SinapsisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uao.edu.co.sinapsis_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinapsisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
